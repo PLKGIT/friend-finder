22719cf47015e0e81ab920b19df32453b7e72f4f
Lammie <plkelly@yahoo.com>
2020-03-11 02:45:31 -0700

    Added modal logic.  Updated README.md.

--- README.md
+++ README.md
@@ -2,19 +2,25 @@
 Homework #11
 
 ## Overview
-TBD
-
-### Features
-TBD
+ Build a compatibility-based "FriendFinder" application -- basically a dating app. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
 
 ### Technologies Used
   * Node JS v12.13.0
   * Express Node Package Manager (NPM) v4.17.1
   * Path NPM v0.12.7
+  * Heroku
+  * HTML
+  * Bootstrap
+  * Google Fonts
 
 ### Using Friend Finder
-TBD
-
+  * Navigate to the home page on Heroku.
+  * Click the "Take the Survey" button in the header.
+  * Complete the survey form and click the Submit button.
+  * After successful form validation, the system will add the user to the friends list.
+  * The app tallies the difference between the user's survey answers and other friends to determine the closest match.
+  * Finally, the system opens a modal containing the name and picture of the survey taker's closest match.
+  Note: There are links on the Home and Survey pages that allow the user to see the complete list of friends in JSON format.
 
 ## Completed Assignment
 
--- app/data/friends.js
+++ app/data/friends.js
@@ -3,19 +3,35 @@
 var friendsArray = [
     {
         "name": "Bob",
-        "photo": "https://dummyimage.com/100x100/1912cc/fff.gif&text=+Bob+",
+        "photo": "https://dummyimage.com/150x150/1912cc/fff.gif&text=+Bobs_Pic+",
         "scores": ["5","5","5","5","5","5","5","5","5","5",]
     },
     {
         "name": "Phil",
-        "photo": "https://dummyimage.com/100x100/1912cc/fff.gif&text=+Phil+",
+        "photo": "https://dummyimage.com/150x150/1912cc/fff.gif&text=+Phils_Pic+",
         "scores": ["3","3","3","3","3","3","3","3","3","3",]
     },
     {
         "name": "John",
-        "photo": "https://dummyimage.com/100x100/1912cc/fff.gif&text=+John+",
+        "photo": "https://dummyimage.com/150x150/1912cc/fff.gif&text=+Johns_Pic+",
         "scores": ["1","1","1","1","1","1","1","1","1","1",]
     },
+    {
+        "name": "Jane",
+        "photo": "https://dummyimage.com/150x150/7F0099/fff.gif&text=+Janes_Pic+",
+        "scores": [4, 3, 2, 4, 1, 5, 4, 2, 3, 5]
+    },
+    {
+        "name": "Sue",
+        "photo": "https://dummyimage.com/150x150/7F0099/fff.gif&text=+Sues_Pic+",
+        "scores": [2, 2, 3, 4, 1, 4, 5, 2, 5, 4]
+    },
+    {
+        "name": "Paula",
+        "photo": "https://dummyimage.com/150x150/7F0099/fff.gif&text=+Paulas_Pic+",
+        "scores": [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+    }
+
 ];
 
 module.exports = friendsArray;
